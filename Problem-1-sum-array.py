b1564cf8a28427e83ad388de3535c148b8a5c1fc
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-11-22 23:35:59 +0530

    Add solution for problem 1

---
+++ Problem-1-sum-array.py
@@ -0,0 +1,33 @@
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+import math
+
+
+# input: int[]
+# k: int
+# returns: true or false
+def sum_pair(input, k):
+    if not input or len(input) < 2:
+        return False
+    hashMap = {}
+    for item in input:
+        diff = k - item
+        if diff in hashMap.keys():
+            return True
+        hashMap[item] = True
+    return False
+
+
+input = [10, 15, 3, 7]
+print(sum_pair(input, 17)) # should be true
+print(sum_pair(input, 12)) # should be false
+print(sum_pair(input, 1))  # should be false
+print(sum_pair([], 200))   # should be false
+print(sum_pair(input, -2)) # should be false
+
+input = [-1, -2, 4, 10, 3]
+print(sum_pair(input, 2))  # should be true
+print(sum_pair(input, 13)) # should be true
+print(sum_pair(input, 13)) # should be true
+print(sum_pair(input, -3)) # should be true
